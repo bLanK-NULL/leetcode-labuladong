641e810f120ad9b44874df47be37b17f1d1a309e
bLanK-NULL <2522385400@qq.com>
2024-04-13 23:23:36 +0800

    update 数组

--- my/快排.js
+++ my/快排.js
@@ -9,7 +9,7 @@ function quickSort(arr) {
     const leftArr = []
     const rightArr = []
     for (let i in arr) { //为什么不行呢？报错堆栈溢出 ： 因为这是String
-    // for (let i = 0; i < len; i++) { // 可以
+        // for (let i = 0; i < len; i++) { // 可以
         if (i == idx) continue;
         const cur = arr[i]
         if (cur < pivot)
@@ -19,7 +19,22 @@ function quickSort(arr) {
     }
     return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
 }
+function quickSort2(arr, i, j) {
+    if (i == j) return;
+    const pre_i = i, pre_j = j;
+    const pivot = arr[i];
+    while (i < j) {
+        while (i < j && pivot <= arr[j]) // 上面是把小于pivot的数放到pivot左边
+            i++;
+        if (i < j)
+            arr[i] = arr[j];
+        while (i < j && pivot > arr[i]) // 最好一个写了等号一个别写
+            j--;
+        if (i < j)
+            arr[j] = arr[i];
+    }
+    arr[i] = pivot;
+    quickSort2(arr, pre_i, i - 1);
+    quickSort2(arr, i + 1, pre_j);
+}
 
-const unsortedArr = [5, 2, 9, 3, 5, 8, 7];
-const sortedArr = quickSort(unsortedArr);
-console.log(sortedArr); // 输出：[2, 3, 5, 5, 7, 8, 9]
